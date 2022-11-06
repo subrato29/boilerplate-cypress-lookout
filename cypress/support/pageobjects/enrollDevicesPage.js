c006947455f242e6e897582cb41622f84ba77b2b
subrato.sarkar <subrato.sarkar@lookout.com>
2022-11-05 23:44:15 -0400

    updated tests

---
+++ cypress/support/pageobjects/enrollDevicesPage.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import BasePage from './basePage'
+
+class EnrollDevicePage extends BasePage {
+
+    get btnEnrollDevices () {
+        return cy.get ('a[class *= \'enroll-device\']');
+    }
+
+    get tabEnrollDevices () {
+        return cy.get ('h1[class *= \'enroll-devices\'] span')
+    }
+
+    get btnEnrollWithEmail () {
+        return cy.get ('button[class *= \'enroll-via-email-button\']');
+    }
+
+    get btnAddEmail () {
+        return cy.get ('button[class *= \'add-email-button\']');
+    }
+
+    get enterEmail () {
+        return cy.get ('textarea[class *= \'text-emails\']');
+    }
+
+    get emailAdded () {
+        return cy.get ('div[class *= \'enrollment-email-editable\'] div:nth-child(1)');
+    }
+
+}
+
+export default new EnrollDevicePage();
--- cypress/support/utils/e2eUtils.js
+++ cypress/support/utils/e2eUtils.js
@@ -15,6 +15,15 @@ var e2eUtils = {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
         return result;
+    },
+    generateRandomString: (length) => {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
     }
 }
 
